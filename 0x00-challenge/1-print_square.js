f9209692ae6b12084e44247c1f275c877d00648b
Nkamenyin <valentineeyet01@gmail.com>
2024-03-16 02:30:34 +0100

    fixcode

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -11,7 +11,7 @@ if (process.argv.length <= 2)
 	process.stderr.write("Example: ./1-print_square.js 8\n");
 	process.exit(1)
 }
-size = parseInt(process.argv[2,10])
+size = parseInt(process.argv[2], 10)
 for (let i = 0 ; i < size ; i ++)
 {
 	for (let j = 0 ; j < size ; j ++)
